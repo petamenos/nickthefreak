f3d68a11b6b7a8e5eed65b02460742370986728c
petamenos <142485011+petamenos@users.noreply.github.com>
2024-01-05 16:04:02 +0200

    Add files via upload

---
+++ hn game.py
@@ -0,0 +1,27 @@
+# hidden number game
+krymenos_arithmos = 57
+prospatheies = 10
+cnt = 0
+for arithmoi in range (0, 100+1):
+    arithmos = int(input('Dwse enan arithmo : '))
+    cnt += 1
+    if cnt >= prospatheies:
+        print("exases")
+        break
+
+    elif arithmos < krymenos_arithmos and arithmos != 0:
+        print ('O arithmos ' +str(arithmos)+ ' einai mikroteros')    
+        continue
+
+    elif arithmos > krymenos_arithmos and arithmos != 0:
+        print('o arithmos ' + str(arithmos) +' einai megalyteros dwse mikrotero')
+        continue
+  
+    elif arithmos == 0:
+        continue
+
+    elif arithmos == krymenos_arithmos and arithmos != 0:
+        print('petyxate ton swsto arithmo!!')
+        break
+
+print("ekanes " + str(cnt) + (" prospatheies"))
