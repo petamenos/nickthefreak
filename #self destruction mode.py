4ae56d3f8f9bf599e08f4b0df8941fc78717fe20
petamenos <142485011+petamenos@users.noreply.github.com>
2024-02-03 13:41:36 +0200

    Add files via upload

---
+++ #self destruction mode.py
@@ -0,0 +1,17 @@
+#self destruction mode
+import time
+print("sir do you want me to initialize self distraction mode?")
+choise = input("yes or no \n")
+if choise == "yes":
+     
+     print("i shall continue")
+elif choise == "no":
+    print("okay then")
+    
+while choise != "no" and choise == "yes":  
+    for second in range(10, 0, -1):
+        print(f"diadikasia autokatastrofhs se {second}")
+        time.sleep(1)#eisagwgh payshs enos deyteroleptoy
+    print("boom!!!")   
+    break
+print("!!!!!")
