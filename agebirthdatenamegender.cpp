ea8404fa2ac37b477617edd800b3a90510f2a640
petamenos <142485011+petamenos@users.noreply.github.com>
2024-03-19 00:37:59 +0200

    Add files via upload

---
+++ agebirthdatenamegender.cpp
@@ -0,0 +1,35 @@
+//statement of name gender and birth date and calculates your age!
+
+#include <iostream>
+using namespace std;
+
+string say_hi(string name)
+{
+    return "hello! " + name;
+}
+int calculateage(int year, int age)
+{
+    return age = 2024-year;
+}
+string genderstatement(string gender)
+{
+    return gender;
+}
+int main()
+{
+    int year, age;
+    string name, gender;
+    
+    cout<<"enter your name please: "<<endl;
+    cin>>onoma;
+    cout<<"please enter your birth date:  "<<endl;
+    cin>>year;
+    cout<<"please enter your gender: "<<endl;
+    cin>>gender;
+    
+    cout<< say_hi(onoma)<<endl;
+    cout<< calculateage(year, age)<<" old"<<endl;
+    cout<< genderstatement(gender);
+    return 0;
+}
+
