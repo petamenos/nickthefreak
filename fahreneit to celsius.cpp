8f67fd466df433056affee9a63c00a7629d4eb88
petamenos <142485011+petamenos@users.noreply.github.com>
2024-03-19 00:06:06 +0200

    Add files via upload

---
+++ fahreneit to celsius.cpp
@@ -0,0 +1,17 @@
+//metatroph thermokrasias apo fahreneit se celsius 
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double f;
+    double c;
+    
+    cout << "dwse thn thermokrasia se fahreneit: " << endl;
+    cin >> f;
+    c = (f - 32) * 5/9;
+    cout << "h thermokrasia se  celsius einai: " << c << endl;
+    return 0;
+}*/
